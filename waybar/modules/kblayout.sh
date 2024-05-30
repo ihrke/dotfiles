6df0e2d33c7d32d514881e1ce5195b93555992c6
Matthias Mittner <matthias.mittner@uit.no>
2024-05-30 16:08:49 +0200

    bugfix

--- waybar/modules/kblayout.sh
+++ waybar/modules/kblayout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hyprctl devices -j |
-  jq -r '.keyboards[] .active_keymap' |
+hyprctl devices -j |   
+	jq -r '.keyboards[] | select(.name=="at-translated-set-2-keyboard") | .active_keymap' |
   head -n1 |
   cut -c1-2 |
   tr 'a-z' 'A-Z'
